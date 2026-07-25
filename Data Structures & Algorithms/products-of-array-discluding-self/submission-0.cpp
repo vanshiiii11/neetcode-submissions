3fe20bf07759cdc5544127bee855d94bc2662b7e
Vanshika Motwani <vanshika.23bce10036@vitbhopal.ac.in>
2026-07-25 09:29:44 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>output(n);
+        for(int i=0;i<n;i++){
+            int prod=1;
+            for(int j=0;j<n;j++){
+                if(i!=j)prod*=nums[j];
+            }
+            output[i]=prod;
+        }
+        return output;
+    }
+};
