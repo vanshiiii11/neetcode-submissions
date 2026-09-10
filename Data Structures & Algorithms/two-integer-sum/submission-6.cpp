4f86a17c5b03652e3da0365c62b93e4294d6f76a
Vanshika Motwani <vanshika.23bce10036@vitbhopal.ac.in>
2026-09-11 00:19:59 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            int needed=target-nums[i];
+            if(mp.count(needed)){
+                return {mp[needed],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
